319cccf4e41a34c7919def61f9b5901990c4de91
Dafydd James <dafydd@cantab.net>
2016-11-06 18:30:27 +0000

    Deleted the unused test_eventbot file.

--- test_eventbot.py
+++
@@ -1,8 +0,0 @@
-# import eventbot
-# from eventbrite_client import EventbriteFacade, MockEventbrite
-#
-# facade = EventbriteFacade(MockEventbrite(None))
-#
-#
-# def test_handle_events_list():
-#     print(eventbot.handle_events_command(facade))
